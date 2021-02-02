a90f51c616b622cf686aa86e5663d541e8ec6c50
sylvianikol <silviaadvert@gmail.com>
2021-02-02 10:19:43 +0200

    passing data with event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,13 +8,20 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('Volcano', 'Chocolate cake', 'https://i2.wp.com/pixahive.com/wp-content/uploads/2020/10/Chocolate-cake-120286-pixahive.jpg')
+    new Recipe('Volcano', 'Chocolate cake', 'https://i2.wp.com/pixahive.com/wp-content/uploads/2020/10/Chocolate-cake-120286-pixahive.jpg'),
+    new Recipe('Strawberry Shortcake', 'Strawberry Shortcake Cake has all the flavors we love in a delicious shortcake, but in cake form!', 'https://www.savingdessert.com/wp-content/uploads/2019/05/Strawberry-Shortcake-Cake-2.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
