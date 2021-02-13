0dabd8b200ae2b6510d44b56c9a79d067b3bcb4a
sylvianikol <silviaadvert@gmail.com>
2021-02-13 12:38:38 +0200

    transformed response data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Recipe } from "../recipes/recipe.model";
+import { map } from 'rxjs/operators';
 
+import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
@@ -24,6 +25,11 @@ export class DataStorageService {
 
     fetchRecipes() {
         this.http.get<Recipe[]>(this.URL + 'recipes.json')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+                });
+            }))
             .subscribe(recipes => {
                 this.recipeService.setRecipes(recipes)
             })
