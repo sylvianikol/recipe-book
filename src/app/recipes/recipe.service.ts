238c7088df1061b05d2f99dc5f91f2df0c31a194
sylvianikol <silviaadvert@gmail.com>
2021-02-13 12:53:55 +0200

    fixed resover bug

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,25 +8,26 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
 
     recipesChanged = new Subject<Recipe[]>();
-
-    private recipes: Recipe[] = [
-        new Recipe('Volcano', 
-        'Chocolate cake', 
-        'https://i2.wp.com/pixahive.com/wp-content/uploads/2020/10/Chocolate-cake-120286-pixahive.jpg', 
-        [
-            new Ingredient('Cacao', 1),
-            new Ingredient('Flour', 1),
-            new Ingredient('Sugar', 2)
-        ]),
-        new Recipe('Strawberry Shortcake', 
-        'Strawberry Shortcake Cake has all the flavors we love in a delicious shortcake, but in cake form!', 
-        'https://www.savingdessert.com/wp-content/uploads/2019/05/Strawberry-Shortcake-Cake-2.jpg', 
-        [
-            new Ingredient('Strawberry', 1),
-            new Ingredient('Milk', 1),
-            new Ingredient('Eggs', 2)
-        ])
-      ];
+    recipes: Recipe[] = [];
+    
+    // private recipes: Recipe[] = [
+    //     new Recipe('Volcano', 
+    //     'Chocolate cake', 
+    //     'https://i2.wp.com/pixahive.com/wp-content/uploads/2020/10/Chocolate-cake-120286-pixahive.jpg', 
+    //     [
+    //         new Ingredient('Cacao', 1),
+    //         new Ingredient('Flour', 1),
+    //         new Ingredient('Sugar', 2)
+    //     ]),
+    //     new Recipe('Strawberry Shortcake', 
+    //     'Strawberry Shortcake Cake has all the flavors we love in a delicious shortcake, but in cake form!', 
+    //     'https://www.savingdessert.com/wp-content/uploads/2019/05/Strawberry-Shortcake-Cake-2.jpg', 
+    //     [
+    //         new Ingredient('Strawberry', 1),
+    //         new Ingredient('Milk', 1),
+    //         new Ingredient('Eggs', 2)
+    //     ])
+    //   ];
 
       constructor(private shoppingListService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
@@ -24,14 +24,14 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>(this.URL + 'recipes.json')
+       
+       return this.http.get<Recipe[]>(this.URL + 'recipes.json')
             .pipe(map(recipes => {
                 return recipes.map(recipe => {
                     return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
                 });
-            }))
-            .subscribe(recipes => {
-                this.recipeService.setRecipes(recipes)
-            })
+            }), tap(recipes => {
+                this.recipeService.setRecipes(recipes);
+            }));
     }
 }
