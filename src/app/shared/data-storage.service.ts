6cc88b1182e944b0fbbdf8bdd9fe29080b0af943
sylvianikol <silviaadvert@gmail.com>
2021-02-13 12:20:34 +0200

    added storing recipes functionallity

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,23 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+import { RecipeService } from "../recipes/recipe.service";
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
+    private URL = 'https://recipe-book-591ec-default-rtdb.europe-west1.firebasedatabase.app/';
 
-    constructor(private http: HttpClient) {
+    constructor(
+        private http: HttpClient,
+        private recipeService: RecipeService
+    ) {}
 
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+
+        this.http.put(this.URL + 'recipes.json', recipes)
+            .subscribe(response => {
+                console.log(response);
+            });
     }
 }
